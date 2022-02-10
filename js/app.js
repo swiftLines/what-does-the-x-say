0720a3f98912704c145f47b4a56fec14069cf4e4
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-10 11:11:04 -0500

    not-fox audio attempt

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const foxImg = document.querySelector('#fox')
+const notFox = document.querySelector('#not-fox')
 const foxSays = new Audio('../audio/fox.mp3')
 
 foxImg.addEventListener('click', (evt) => {
   foxSays.volume = .10
   foxSays.play()
+})
+
+notFoxImg.addEventListener('click', (evt) => {
+  if (evt.target.id !== 'not-fox') {
+    const audioElement = new Audio(`../audio/${evt.target.id}.mp3`)
+    audioElement.volume = .1
+    audioElement.play()
+  }
 })
